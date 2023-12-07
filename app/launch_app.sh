5bd8e2dcf47013564f4ee62f41c4682372ca6a9f
Dominic Cicilio <dominic@yoamigo.io>
2023-12-06 23:52:47 -0500

    deletes unused files

--- app/launch_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/bin/echo "Script started" >> /tmp/launch_app_log.txt
-/Users/dominic.cicilio/Documents/repos/github-video-compressor/app/node_modules/.bin/electron /Users/dominic.cicilio/Documents/repos/github-video-compressor/app/node_modules/.dev/main/index.js
-/bin/echo "Script ended" >> /tmp/launch_app_log.txt
