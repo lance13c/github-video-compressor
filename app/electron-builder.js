64f6bbcf82d28ad88027f7ad4b6fa3364ef44702
Dominic Cicilio <dominic@yoamigo.io>
2024-01-02 03:47:24 -0500

    fix electron build

--- app/electron-builder.js
+++ app/electron-builder.js
@@ -1,12 +1,12 @@
-const { main, name, version, resources, description, displayName, author: _author } = require('./package.json');
+const { main, name, version, resources, description, displayName, author: _author } = require('./package.json')
 
-const { getDevFolder } = require('./bin/utils');
+const { getDevFolder } = require('./bin/utils')
 
-const author = _author?.name ?? _author;
-const currentYear = new Date().getFullYear();
-const authorInSnakeCase = author.replace(/\s+/g, '_');
-const appId = `com.${authorInSnakeCase}.${name}`.toLowerCase();
-console.log('appId: ' + appId);
+const author = _author?.name ?? _author
+const currentYear = new Date().getFullYear()
+const authorInSnakeCase = author.replace(/\s+/g, '_')
+const appId = `com.${authorInSnakeCase}.${name}`.toLowerCase()
+console.log('appId: ' + appId)
 
 /** @type {import('electron-builder').Configuration} */
 module.exports = {
@@ -25,17 +25,17 @@ module.exports = {
   },
 
   dmg: {
-    icon: false,
+    icon: true,
   },
 
-  // linux: {
-  //   category: 'Utilities',
-  //   synopsis: description,
-  //   target: ['AppImage', 'deb', 'pacman', 'freebsd', 'rpm'],
-  // },
+  linux: {
+    category: 'Utilities',
+    synopsis: description,
+    target: ['AppImage', 'deb', 'pacman', 'freebsd', 'rpm'],
+  },
 
-  // win: {
-  //   icon: `${resources}/build/icons/icon.ico`,
-  //   target: ['nsis', 'portable', 'zip'],
-  // },
-};
+  win: {
+    icon: `${resources}/build/icons/icon.ico`,
+    target: ['nsis', 'portable', 'zip'],
+  },
+}
