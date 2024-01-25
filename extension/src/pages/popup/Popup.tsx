ac804a772ce1932bb1360ad95fef8f667fd83ac2
Dominic Cicilio <dominic@yoamigo.io>
2024-01-25 15:23:50 -0500

    clean up

--- extension/src/pages/popup/Popup.tsx
+++ extension/src/pages/popup/Popup.tsx
@@ -1,25 +1,23 @@
-import logo from '@assets/img/logo.svg';
-import '@pages/popup/Popup.css';
-import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
-import withSuspense from '@src/shared/hoc/withSuspense';
-import useStorage from '@src/shared/hooks/useStorage';
-import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
+import logo from '@assets/img/icon-32.png'
+import '@pages/popup/Popup.css'
+import withErrorBoundary from '@src/shared/hoc/withErrorBoundary'
+import withSuspense from '@src/shared/hoc/withSuspense'
+import useStorage from '@src/shared/hooks/useStorage'
+import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage'
 
 const Popup = () => {
-  const theme = useStorage(exampleThemeStorage);
+  const theme = useStorage(exampleThemeStorage)
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#fff' : '#000',
-      }}>
-      <header className="App-header" style={{ color: theme === 'light' ? '#000' : '#fff' }}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
+    <div className="App">
+      <header className="App-header">
+        <div className="App-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <p>Github Video Compressor</p>
       </header>
     </div>
-  );
-};
+  )
+}
 
-export default withErrorBoundary(withSuspense(Popup, <div> Loading ... </div>), <div> Error Occur </div>);
+export default withErrorBoundary(withSuspense(Popup, <div> Loading ... </div>), <div> Error Occur </div>)
