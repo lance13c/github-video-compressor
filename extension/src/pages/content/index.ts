02cb5b033509d3720803add9d6e6ffd9f8dd91a3
Dominic Cicilio <dominic@yoamigo.io>
2024-01-25 15:03:52 -0500

    only runs content on github, prevents targetNode missing errors

--- extension/src/pages/content/index.ts
+++ extension/src/pages/content/index.ts
@@ -10,6 +10,11 @@
     handleTextAreaDrop,
   } = await import('@root/src/pages/content/domains/github/ui.util')
 
+  const isValidDomain = (): boolean => {
+    const domain = window.location.hostname
+    return domain === 'github.com'
+  }
+
   const resetAllFileUploadListeners = () => {
     const fileInputs = document.querySelectorAll('input[type=file]')
     clearAllDomEventListeners()
@@ -32,25 +37,28 @@
     })
   }
 
-  // Select the node that will be observed for mutations
-  const targetNode = document.querySelector('.pull-discussion-timeline')
+  if (isValidDomain) {
+    // Callback function to execute when mutations are observed
+    const callback = function (mutationsList) {
+      if (mutationsList.length > 0) {
+        // When any mutations are observed, add listeners to the file inputs
+        resetAllFileUploadListeners()
+      }
+    }
 
-  // Callback function to execute when mutations are observed
-  const callback = function (mutationsList) {
-    if (mutationsList.length > 0) {
-      // When any mutations are observed, add listeners to the file inputs
+    // Create an instance of MutationObserver
+    const observer = new MutationObserver(callback)
+
+    // Select the node that will be observed for mutations
+    const targetNode = document.querySelector('.pull-discussion-timeline')
+
+    // Start observing the target node for configured mutations
+    if (observer && targetNode) {
+      observer.observe(targetNode, {
+        childList: true,
+        subtree: true,
+      })
       resetAllFileUploadListeners()
     }
   }
-
-  // Create an instance of MutationObserver
-  const observer = new MutationObserver(callback)
-
-  // Start observing the target node for configured mutations
-  observer.observe(targetNode, {
-    childList: true,
-    subtree: true,
-  })
-
-  resetAllFileUploadListeners()
 })()
