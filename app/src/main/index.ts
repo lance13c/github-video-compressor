7a9e917909edffee49863f7bf4d6242d3deabe32
Dominic Cicilio <dominic@yoamigo.io>
2024-04-08 21:06:21 -0400

    removes spaces from names, trying a different way to detect if application is starting from native messaging

--- app/src/main/index.ts
+++ app/src/main/index.ts
@@ -16,57 +16,61 @@ makeAppWithSingleInstanceLock(async () => {
   const isDev = process.argv.includes('--development')
   sendDebugMessage('info', `isDev: ${isDev}`)
 
-  const isNativeMessaging = process.argv.includes('--native-messaging')
-  sendDebugMessage('info', `isNativeMessaging: ${isNativeMessaging}`)
+  let isNativeMessaging = false
+  const nativeMessagingHost = new NativeMessagingHost()
 
   try {
-    // Do not launch the app UI and setup if the app is running via native messaging
-    if (!isNativeMessaging) {
-      await checkSetup(app)
-      process.stdin.on('end', () => {
-        sendDebugMessage('info', 'stdin closed, shutting down Electron app')
-        app.quit()
-      })
-    } else {
-      const nativeMessagingHost = new NativeMessagingHost()
-      if (isDev) {
-        // initWebSocketServer()
-      }
-
-      const { server } = startHttpFileServer(app, port)
-
-      if (!server) {
-        sendDebugMessage('error', 'Failed to start HTTP file server')
-        app.quit()
-        return
-      }
-
-      nativeMessagingHost.addListener(message => {
-        sendDebugMessage('debug', `Received message ${JSON.stringify(message)}`)
-        if (message.type === 'connection') {
-          sendDebugMessage('debug', 'Init connection - Received connection request')
-
-          const token = generateToken({
-            // generate random client uuid
-            clientId: uuidv4(),
-          })
-
-          nativeMessagingHost.sendMessage({
-            type: 'connection',
-            progress: 1,
-            data: token,
-          })
-        }
-      })
-
-      sendDebugMessage('info', 'Electron app started')
-
-      process.stdin.on('end', () => {
-        sendDebugMessage('info', 'stdin closed, shutting down Electron app')
-        server.close()
-        app.quit()
-      })
+    if (isDev) {
+      // initWebSocketServer()
     }
+
+    nativeMessagingHost.addListener(message => {
+      sendDebugMessage('debug', `Received message ${JSON.stringify(message)}`)
+      if (message.type === 'connection') {
+        isNativeMessaging = true
+        sendDebugMessage('debug', 'Init connection - Received connection request')
+
+        const token = generateToken({
+          // generate random client uuid
+          clientId: uuidv4(),
+        })
+
+        nativeMessagingHost.sendMessage({
+          type: 'connection',
+          progress: 1,
+          data: token,
+        })
+      }
+    })
+
+    // Delay main execution to allow to see if it is the native messaging connection or not.
+    await setTimeout(async () => {
+      sendDebugMessage('info', 'Is native messaging: ' + isNativeMessaging)
+      // Do not launch the app UI and setup if the app is running via native messaging
+      if (!isNativeMessaging) {
+        await checkSetup(app)
+        process.stdin.on('end', () => {
+          sendDebugMessage('info', 'stdin closed, shutting down Electron app')
+          app.quit()
+        })
+      } else {
+        const { server } = startHttpFileServer(app, port)
+
+        if (!server) {
+          sendDebugMessage('error', 'Failed to start HTTP file server')
+          app.quit()
+          return
+        }
+
+        sendDebugMessage('info', 'Electron app started')
+
+        process.stdin.on('end', () => {
+          sendDebugMessage('info', 'stdin closed, shutting down Electron app')
+          server.close()
+          app.quit()
+        })
+      }
+    }, 2000)
   } catch (e) {
     // @ts-ignore
     sendDebugMessage('error', e?.message || 'unknown')
